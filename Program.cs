1cb5dc921afd31d4732ff260f81d2c5bc255c0af
dangvantoan1407 <toandvth2209025@fpt.edu.vn>
2023-08-17 15:48:31 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ASM.asm3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount();
+
+
+        account.Deposit(1000);
+        account.Withdraw(500);
+
+
+    }
+
+}
---
+++ asm3/BankAccount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM.asm3
+{
+    public delegate void BalanceChangedEventHandler(double newBalance);
+
+    public class BankAccount
+    {
+
+        private double balance;
+        public event BalanceChangedEventHandler BalanceChanged;
+
+        public BankAccount()
+        {
+            BalanceChanged += HandleBalanceChanged;
+
+        }
+        public double Balance
+        {
+            get => balance;
+            private set
+            {
+                if (balance != value)
+                {
+                    balance = value;
+
+                }
+            }
+        }
+        public void Withdraw(double amout)
+        {
+            if (amout > 0 && amout <= balance)
+            {
+                balance -= amout;
+                BalanceChanged(amout);
+            }
+        }
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                BalanceChanged(amount);
+            }
+        }
+
+
+        public void HandleBalanceChanged(double newBalance)
+        {
+            Console.WriteLine($"Balance changed. New balance: {newBalance}");
+        }
+
+
+    }
+}
